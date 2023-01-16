3d0a4461b61f39171a8c633765b3cdd9319293ab
nyza-cabillo <cabillonyza@gmail.com>
2023-01-17 01:07:04 +0800

    Attached Login button

--- resources/views/homepage/includes/nav.blade.php
+++ resources/views/homepage/includes/nav.blade.php
@@ -13,6 +13,7 @@
                         <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
                         <li class="nav-item"><a class="nav-link" href="#team">Team</a></li>
                         <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ url('/login') }}">Login</a></li>
                     </ul>
                 </div>
             </div>

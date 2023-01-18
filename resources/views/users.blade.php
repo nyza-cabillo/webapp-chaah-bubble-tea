a139ca6ad4909eb26e86bdf0b29ad79690d8f06c
nyza-cabillo <cabillonyza@gmail.com>
2023-01-18 13:24:13 +0800

    Adding Users in Login Page

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Users') }}
         </h2>
     </x-slot>
 
@@ -10,7 +10,7 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     
-                <h1> Hi! Welcome to Cha'ah Bubble Tea {{ Auth::user()->name}} </h1>
+               
 
 
                 </div>
